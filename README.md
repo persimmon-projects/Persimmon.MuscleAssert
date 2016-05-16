4b8f7470a921049b77a8f2797dee35a8562e8458
pocketberserker <pocketberserker@gmail.com>
2016-05-17 03:41:18 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Persimmon.Assertion
+
---
+++ src/Persimmon.Assertion/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitleAttribute("Persimmon.Assertion")>]
+[<assembly: AssemblyProductAttribute("Persimmon.Assertion")>]
+[<assembly: GuidAttribute("42384685-8f4f-45d7-a305-0528113e1c19")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] InformationalVersion = "0.1.0"
