2568aee5c02c9d3262c8bc790ca64964dc259b68
pocketberserker <pocketberserker@gmail.com>
2016-05-27 13:19:44 +0900

    Bump version to 0.2.0-beta

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,8 @@
+### 0.2.0-beta - May 27 2016
+* fix target dependencies(FSharp.Object.Diff >= 0.5.2, Persimmon >= 1.1.0)
+* change diff prefix
+* merge difference of same path
+* show chaining style path
+
 ### 0.1.0-beta - May 26 2016
 * initial release
--- src/Persimmon.Assertion/AssemblyInfo.fs
+++ src/Persimmon.Assertion/AssemblyInfo.fs
@@ -6,11 +6,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("Persimmon.Assertion")>]
 [<assembly: GuidAttribute("42384685-8f4f-45d7-a305-0528113e1c19")>]
 [<assembly: AssemblyDescriptionAttribute("")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0-beta")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0-beta")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0-beta"
+    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] InformationalVersion = "0.2.0-beta"
