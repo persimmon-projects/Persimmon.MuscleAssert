c2f30ccef130df9d2c3c750a7144f5a167798874
pocketberserker <pocketberserker@gmail.com>
2022-03-06 19:31:28 +0900

    support netstandard2.0 or higher

--- src/Persimmon.MuscleAssert/TypeHelper.fs
+++ src/Persimmon.MuscleAssert/TypeHelper.fs
@@ -1,23 +1,13 @@
 ﻿module internal Persimmon.Type
 
 open System
-#if NETSTANDARD
 open System.Reflection
-#endif
 
 let rec name (t: Type) =
-#if NETSTANDARD
   let info = t.GetTypeInfo()
   if info.IsGenericType then
-#else
-  if t.IsGenericType then
-#endif
     let n = t.Name.Split([|'`'|]).[0]
-#if NETSTANDARD
     info.GenericTypeArguments
-#else
-    t.GetGenericArguments()
-#endif
     |> Array.map name
     |> String.concat ", "
     |> sprintf "%s<%s>" n
